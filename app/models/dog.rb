f35183397c7d2dddcbcecad4a550d2851df7d325
Justin Hall <hall.justin.d@gmail.com>
2019-04-27 18:31:35 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,16 @@
 class dog
+  attr_accessor :name, :breed, :age
+
+  @dogs = []
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @dogs << self
+  end
+
+  def self.all
+    @all
+  end
 end
